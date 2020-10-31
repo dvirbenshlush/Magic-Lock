5c86fd36031eabae12b33208d3a5833debb47ae7
dvirbsh95 <dvirbsh95@gmail.com>
2020-10-31 03:10:33 +0200

    add a libarary

---
+++ frontend/our-app/src/components/about.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+
+const SERVER_ADDRESS = process.env.NODE_ENV === "development" ? 'http://localhost:5000' : "";
+
+
+export class about extends Component {
+
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    fetch(`${SERVER_ADDRESS}/about_us`)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ data: data.data });
+        console.log(data);
+      });
+  }
+
+  render() {
+    return (
+      <div id="about">
+        <div className="container">
+          <div className="row">
+            <div className="col-xs-12 col-md-6"> <img src="img/about.jpg" className="img-responsive" alt="" /> </div>
+            <div className="col-xs-12 col-md-6">
+              <div className="about-text">
+                <h2>About Us</h2>
+                <p>{this.props.data ? this.props.data.paragraph : 'loading...'}</p>
+                <h3>Why Choose Us?</h3>
+                <div className="list-style">
+                  <div className="col-lg-6 col-sm-6 col-xs-12">
+                    <ul>
+                      {this.props.data ? this.props.data.Why.map((d, i) => <li key={`${d}-${i}`}>{d}</li>) : 'loading'}
+                    </ul>
+                  </div>
+                  <div className="col-lg-6 col-sm-6 col-xs-12">
+                    <ul>
+                      {this.props.data ? this.props.data.Why2.map((d, i) => <li key={`${d}-${i}`}> {d}</li>) : 'loading'}
+
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default about
