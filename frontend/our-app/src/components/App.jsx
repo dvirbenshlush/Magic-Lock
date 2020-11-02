7904d8a39ee0a9dcfa9bad281b338fed5bd0d96c
dvirbsh95 <dvirbsh95@gmail.com>
2020-11-02 04:43:55 +0200

    changing the navbar style and add a link to call

--- frontend/our-app/src/components/App.jsx
+++ frontend/our-app/src/components/App.jsx
@@ -38,6 +38,12 @@ export class App extends Component {
         <About  data={this.state.resumeData.About}/>
         <Gallery />
         <Contact  data={this.state.resumeData.Contact}/>
+
+        {/* <div class="boxfloatbottom">
+        <a href="tel:7045089300" style="float:left;margin-left: 20px;" title="">    </a>
+        <a href="https://api.whatsapp.com/send?phone=9549977744" style="float:left;margin-left: 20px;" title="">            <p style="display: inline-block;border-radius: 60px;box-shadow: 3px 3px 5px #888;color: #fff;background-color: #49da67;width: 70px;height: 70px;border: 2px solid #fff;font-size: 35px;padding: 0 20px;">                <i class="icon ion-social-whatsapp" style="color:#fff;"></i>            </p>        </a>
+        <a href="https://waze.com/ul?q={355 W Martin Luther King Jr Blvd, Charlotte, North Carolina}" style="float:left;margin-left: 20px;" title="">            <p style="display: inline-block;border-radius: 60px;box-shadow: 3px 3px 5px #888;color: #fff;background-color: #41d5ff;width: 70px;height: 70px;border: 2px solid #fff;font-size: 35px;padding: 0 20px;">                <i class="icon ion-android-pin" style="color:#fff;"></i>            </p>        </a>
+    </div> */}
       </div>
     )
   }
--- frontend/our-app/src/components/navigation.jsx
+++ frontend/our-app/src/components/navigation.jsx
@@ -28,22 +28,13 @@ export class Navigation extends Component {
             className="collapse navbar-collapse"
             id="bs-example-navbar-collapse-1"
           >
+            
             <ul className="nav navbar-nav navbar-right">
             
-              <li>
-                {/* <a href="#features" className="page-scroll">
-                  Features
-                </a> */}
-              </li>
               <li>
                 <a href="#about" className="page-scroll">
                   About
                 </a>
-              </li>
-              <li>
-                {/* <a href="#services" className="page-scroll">
-                  Services
-                </a> */}
               </li>
               <li>
                 <a href="#portfolio" className="page-scroll">
@@ -51,21 +42,17 @@ export class Navigation extends Component {
                 </a>
               </li>
               <li>
-                {/* <a href="#testimonials" className="page-scroll">
-                  Testimonials
-                </a> */}
-              </li>
-              <li>
-                {/* <a href="#team" className="page-scroll">
-                  Team
-                </a> */}
-              </li>
-              <li>
                 <a href="#contact" className="page-scroll">
                   Contact
                 </a>
               </li>
-            
+              <li>
+            <a href="tel:7045089300">
+                    <span>Call Us</span>
+                    <br/>
+                    704-508-9300
+                    </a>
+                    </li>
             </ul>
             <img src= "img/magic-lock-logo.png" alt="logo"   />
           </div>
